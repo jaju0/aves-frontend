d9fea0c1e175c598a59700b86fae7ebd524ac767
jaju0 <jannes_oltmanns@hotmail.de>
2024-06-20 00:26:46 +0200

    added chart page

---
+++ src/pages/ChartPage/index.tsx
@@ -0,0 +1,33 @@
+import { KlineIntervalV3 } from "bybit-api";
+import { createContext, useEffect, useState } from "react";
+
+export interface SymbolPair
+{
+    symbol1: string;
+    symbol2: string;
+    interval: KlineIntervalV3;
+}
+
+export const SymbolPairContext = createContext<[SymbolPair, React.Dispatch<React.SetStateAction<SymbolPair>>]>({} as [SymbolPair, React.Dispatch<React.SetStateAction<SymbolPair>>]);
+
+export function ChartPage()
+{
+    const localSymbolPair = JSON.parse(localStorage.getItem("symbolPair") ?? "{}");
+    const [symbolPair, setSymbolPair] = useState<SymbolPair>(localSymbolPair);
+
+    useEffect(() => {
+        localStorage.setItem("symbolPair", JSON.stringify(symbolPair));
+    }, [symbolPair]);
+
+    return (
+        <SymbolPairContext.Provider value={[symbolPair, setSymbolPair]}>
+            <div className="h-full">
+                <div className="h-full grid grid-rows-12 grid-cols-4 gap-1 px-5">
+                    <div className="row-span-1 col-span-4">
+                    </div>
+                </div>
+            </div>
+        </SymbolPairContext.Provider>
+    );
+}
+
