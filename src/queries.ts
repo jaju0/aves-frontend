4cfb0dbf5e9065d65281fda6aa9980be4058532b
jaju0 <jannes_oltmanns@hotmail.de>
2024-06-19 12:44:56 +0200

    added bybit API queries
    - instrument info query
    - kline query

--- src/queries.ts
+++ src/queries.ts
@@ -1,4 +1,5 @@
 import axios, { AxiosError } from "axios";
+import { APIResponseV3WithTime, CategorySymbolListV5, InstrumentInfoResponseV5, KlineIntervalV3, OHLCVKlineV5, RestClientV5 } from "bybit-api";
 import { useJwtData } from "./hooks/useJwtData";
 
 export const userRank = ["ADMIN", "USER", "NONE"];
@@ -259,3 +260,33 @@ export const userDeletionMutation = {
         }
     }
 }
+export const bybitInstrumentInfoQuery = (bybitRestClient: RestClientV5) => ({
+    staleTime: Infinity,
+    queryKey: ["bybit", "instrumentInfo"],
+    queryFn: async () => {
+        const response = await bybitRestClient.getInstrumentsInfo({
+            category: "linear",
+        });
+
+        return response;
+    },
+});
+
+export type BybitInstrumentInfoQueryResponse = APIResponseV3WithTime<InstrumentInfoResponseV5<"linear">>;
+
+export const bybitKlineQuery = (bybitRestClient: RestClientV5, symbol: string, interval: KlineIntervalV3) => ({
+    staleTime: typeof (+interval) === "number" ? Date.now() % ((+interval) * 60 * 1000) : Infinity,
+    queryKey: ["bybit", "kline", symbol, interval],
+    queryFn: async () => {
+        const response = await bybitRestClient.getKline({
+            category: "linear",
+            interval: interval,
+            symbol: symbol,
+            limit: 1000,
+        });
+
+        return response;
+    },
+});
+
+export type BybitKlineQueryResponse = APIResponseV3WithTime<CategorySymbolListV5<OHLCVKlineV5[], "spot" | "linear" | "inverse">>;
